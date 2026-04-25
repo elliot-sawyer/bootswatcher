2772e0b35c2d67b9904d0f0b3e3a16e7acb7105e
elliot sawyer <elliot@cashware.nz>
2026-04-25 18:43:42 +1200

    Add BootswatchThemeField image picker and thumbnail downloader
    Replaces the plain DropdownField in SiteConfig with a custom
    BootswatchThemeField subclass that renders theme preview thumbnails
    in a grid-style picker. Thumbnails are downloaded locally by a new
    getThumbnails() method in BootswatchDownloader (called on dev/build)
    so images are served from _resources rather than hot-linked.

--- app/src/Extensions/SiteConfigTheme.php
+++ app/src/Extensions/SiteConfigTheme.php
@@ -1,8 +1,8 @@
 <?php
 namespace Cashware\Bootswatcher;
 
+use Cashware\Bootswatcher\Forms\BootswatchThemeField;
 use SilverStripe\Core\Extension;
-use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\View\Requirements;
 
@@ -18,7 +18,7 @@ class SiteConfigTheme extends Extension
     public function updateCMSFields(FieldList $fields): void
     {
         $fields->addFieldsToTab('Root.Theme', [
-            DropdownField::create('Theme', 'Bootswatch Theme')
+            BootswatchThemeField::create('Theme', 'Bootswatch Theme')
                 ->setSource(BootswatchDownloader::config()->bootswatch_themes)
         ]);
     }
@@ -39,6 +39,7 @@ class SiteConfigTheme extends Extension
         $task = new BootswatchDownloader();
         $task->getCSS();
         $task->getJS();
+        $task->getThumbnails();
     }
 
     /**
